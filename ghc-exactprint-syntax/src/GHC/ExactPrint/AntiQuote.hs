7ec13e2accae2c208abe9824a14d4d2983049b93
Isaac Elliott <isaace71295@gmail.com>
2024-10-29 06:43:55 +1000

    Add substModuleName

--- ghc-exactprint-syntax/src/GHC/ExactPrint/AntiQuote.hs
+++ ghc-exactprint-syntax/src/GHC/ExactPrint/AntiQuote.hs
@@ -1,16 +1,20 @@
 module GHC.ExactPrint.AntiQuote where
 
-import GHC (GenLocated (..), GhcPs, HsExpr (..))
-import GHC.ExactPrint.QQ (ParsedExpr)
+import Data.Data (Data)
+import Data.Maybe (fromMaybe)
+import GHC (GenLocated (..), GhcPs, HsExpr (..), ModuleName, moduleNameString)
 import Generics.SYB (everywhere, mkT)
 import Language.Haskell.GHC.ExactPrint.Utils (rdrName2String)
 
-substExpr :: [(String, ParsedExpr)] -> ParsedExpr -> ParsedExpr
+substExpr :: Data a => [(String, HsExpr GhcPs)] -> a -> a
 substExpr vars = everywhere (mkT f)
   where
     f :: HsExpr GhcPs -> HsExpr GhcPs
-    f a@(HsVar _ (L _ ident)) =
-      case lookup (rdrName2String ident) vars of
-        Nothing -> a
-        Just (L _ a') -> a'
+    f a@(HsVar _ (L _ ident)) = fromMaybe a $ lookup (rdrName2String ident) vars
     f a = a
+
+substModuleName :: Data a => [(String, ModuleName)] -> a -> a
+substModuleName vars = everywhere (mkT f)
+  where
+    f :: ModuleName -> ModuleName
+    f a = fromMaybe a $ lookup (moduleNameString a) vars

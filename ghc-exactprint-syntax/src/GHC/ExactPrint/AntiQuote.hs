1c16e0b1cabb966ae12eac2429d28456249bb8dd
Isaac Elliott <isaace71295@gmail.com>
2024-10-29 07:25:58 +1000

    Add substDoStmts

--- ghc-exactprint-syntax/src/GHC/ExactPrint/AntiQuote.hs
+++ ghc-exactprint-syntax/src/GHC/ExactPrint/AntiQuote.hs
@@ -2,7 +2,17 @@ module GHC.ExactPrint.AntiQuote where
 
 import Data.Data (Data)
 import Data.Maybe (fromMaybe)
-import GHC (GenLocated (..), GhcPs, HsExpr (..), ModuleName, moduleNameString)
+import GHC
+  ( ExprLStmt
+  , GenLocated (..)
+  , GhcPs
+  , HsExpr (..)
+  , LHsExpr
+  , ModuleName
+  , NoExtField (..)
+  , StmtLR (..)
+  , moduleNameString
+  )
 import Generics.SYB (everywhere, mkT)
 import Language.Haskell.GHC.ExactPrint.Utils (rdrName2String)
 
@@ -18,3 +28,15 @@ substModuleName vars = everywhere (mkT f)
   where
     f :: ModuleName -> ModuleName
     f a = fromMaybe a $ lookup (moduleNameString a) vars
+
+substDoStmts :: Data a => [(String, [StmtLR GhcPs GhcPs (LHsExpr GhcPs)])] -> a -> a
+substDoStmts vars = everywhere (mkT f)
+  where
+    f :: HsExpr GhcPs -> HsExpr GhcPs
+    f (HsDo ext flavour (L loc stmts)) = HsDo ext flavour . L loc $ g =<< stmts
+    f a = a
+
+    g :: ExprLStmt GhcPs -> [ExprLStmt GhcPs]
+    g (L loc (BodyStmt NoExtField (L _ (HsVar _ (L _ ident))) NoExtField NoExtField))
+      | Just stmts <- lookup (rdrName2String ident) vars = L loc <$> stmts
+    g a = [a]
